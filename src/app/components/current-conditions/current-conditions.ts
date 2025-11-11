86e021427a38f06d8f07c735f63b28d2b57ea8b7
Netsky2-tech <octaviomr9810@gmail.com>
2025-11-11 15:51:07 -0600

    feat: add animation to tabs content to improve de ux

--- src/app/components/current-conditions/current-conditions.ts
+++ src/app/components/current-conditions/current-conditions.ts
@@ -1,8 +1,6 @@
 import { Component, Input, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
-import { Observable } from 'rxjs';
-import { WeatherApi } from '../../services/weather-api/weather-api';
 import {  CurrentDataEntity, CurrentWeatherModel } from '../../models/current-weather.model';
 
 @Component({
@@ -12,13 +10,9 @@ import {  CurrentDataEntity, CurrentWeatherModel } from '../../models/current-we
   styleUrl: './current-conditions.scss',
 })
 export class CurrentConditions {
-
   // Se recibe el zipcode como Input, se usa definite assignment assertion,
   // ya que el valor sera inyectado por Angular, no dentro del constructor
   @Input({ required: true }) zipcode!: string;
 
-
-
   @Input({ required: true }) conditions: CurrentDataEntity | null = null;
-
 }
